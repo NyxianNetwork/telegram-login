5ce84654f8848be77195ff84e456593c907811e4
NyxianNetwork <nyxiannetwork@gmail.com>
2024-08-15 19:34:02 +0700

    Update login.py

--- login.py
+++ login.py
@@ -1,6 +1,6 @@
 from pyrogram import Client
 
-# Masukkan string sesi Telegram kamu di sini
+# Masukkan string sesi yang sudah kamu miliki
 session_string = "BQGE4bMAW9xGCxFSlHg2g793J_9p2ZwHMmpDicrpa-UTSqCZvJG_fW35dpgkVMyEPtQwYH0YaVZ0X-je0UK4Tpkf78SIlnDtwUycZGPq2vcb4-PDJAxaX62L2eE8KxHBwmx3MpfUmhW85ci6-mTDdSkB3bX68ChodFQvgsFpoV0Phc86wh70LHNA_bRkLIdisgI1GCqHKiR_7ulYp20eqI3wu6XFtr-jlUCvii7PtWB1smhuv8voEOr8UOAhw6UXjgItMMw7ssf_jDzAUudWphCyBL2qBRPncBwEqn5bWFCx02Vc0aVBgo4QnGWAWrVFzita5-ca8o9iSo1PEaOnzEVLZqtg5gAAAAFfpka9AA"
 
 # Buat objek Client dengan menggunakan string sesi
@@ -10,4 +10,6 @@ app = Client("my_account", session_string=session_string)
 with app:
     # Dapatkan informasi akun yang sedang login
     me = app.get_me()
-    print(me)
+
+    # Tampilkan nomor telepon
+    print("Nomor Telepon Anda:", me.phone_number)
