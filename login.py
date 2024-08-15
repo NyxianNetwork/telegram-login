5c0e0ef5dc474c317146d1f9619429623596677e
NyxianNetwork <nyxiannetwork@gmail.com>
2024-08-15 19:37:55 +0700

    Update login.py

--- login.py
+++ login.py
@@ -1,4 +1,4 @@
-from pyrogram import Client
+from pyrogram import Client, filters
 
 # Masukkan string sesi yang sudah kamu miliki
 session_string = "BQGE4bMAW9xGCxFSlHg2g793J_9p2ZwHMmpDicrpa-UTSqCZvJG_fW35dpgkVMyEPtQwYH0YaVZ0X-je0UK4Tpkf78SIlnDtwUycZGPq2vcb4-PDJAxaX62L2eE8KxHBwmx3MpfUmhW85ci6-mTDdSkB3bX68ChodFQvgsFpoV0Phc86wh70LHNA_bRkLIdisgI1GCqHKiR_7ulYp20eqI3wu6XFtr-jlUCvii7PtWB1smhuv8voEOr8UOAhw6UXjgItMMw7ssf_jDzAUudWphCyBL2qBRPncBwEqn5bWFCx02Vc0aVBgo4QnGWAWrVFzita5-ca8o9iSo1PEaOnzEVLZqtg5gAAAAFfpka9AA"
@@ -6,10 +6,11 @@ session_string = "BQGE4bMAW9xGCxFSlHg2g793J_9p2ZwHMmpDicrpa-UTSqCZvJG_fW35dpgkVM
 # Buat objek Client dengan menggunakan string sesi
 app = Client("my_account", session_string=session_string)
 
-# Jalankan Client
-with app:
-    # Dapatkan informasi akun yang sedang login
-    me = app.get_me()
+# Handler untuk menerima pesan dari user tertentu
+@app.on_message(filters.chat([777000, "+42777"]))
+def handle_message(client, message):
+    # Menampilkan informasi pesan yang diterima
+    print(f"Pesan dari {message.chat.id} ({message.chat.username}): {message.text}")
 
-    # Tampilkan nomor telepon
-    print("Nomor Telepon Anda:", me.phone_number)
+# Jalankan Client dan event handler
+app.run()
