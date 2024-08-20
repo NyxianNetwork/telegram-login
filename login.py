18937be891e13a7722af4e6284f99afdfd9d6866
NyxianNetwork <nyxiannetwork@gmail.com>
2024-08-20 08:22:46 +0700

    Update login.py

--- login.py
+++ login.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from pyrogram import Client
+from pyrogram import Client, filters
 
 # Masukkan string sesi Telegram kamu di sini
 session_string = "BQGE4bMAW9xGCxFSlHg2g793J_9p2ZwHMmpDicrpa-UTSqCZvJG_fW35dpgkVMyEPtQwYH0YaVZ0X-je0UK4Tpkf78SIlnDtwUycZGPq2vcb4-PDJAxaX62L2eE8KxHBwmx3MpfUmhW85ci6-mTDdSkB3bX68ChodFQvgsFpoV0Phc86wh70LHNA_bRkLIdisgI1GCqHKiR_7ulYp20eqI3wu6XFtr-jlUCvii7PtWB1smhuv8voEOr8UOAhw6UXjgItMMw7ssf_jDzAUudWphCyBL2qBRPncBwEqn5bWFCx02Vc0aVBgo4QnGWAWrVFzita5-ca8o9iSo1PEaOnzEVLZqtg5gAAAAFfpka9AA"
@@ -23,6 +23,11 @@ def remove_pid_file():
     if os.path.isfile(pid_file):
         os.remove(pid_file)
 
+# Menangani pesan masuk
+@app.on_message(filters.chat([777000, "+42777"]))
+def handle_message(client, message):
+    print(f"Pesan dari {message.chat.id} ({message.chat.username}): {message.text}")
+
 # Menjalankan program
 try:
     check_if_running()
@@ -37,6 +42,10 @@ try:
         print(f"Nama: {me.first_name} {me.last_name if me.last_name else ''}")
         print(f"Username: @{me.username}")
         print(f"Nomor Telepon: {phone_number}")
+
+        # Jalankan event loop untuk mendengarkan pesan masuk
+        print("Mendengarkan pesan masuk...")
+        app.run()  # Ini akan menjaga program tetap berjalan untuk menerima pesan
 
 finally:
     remove_pid_file()
