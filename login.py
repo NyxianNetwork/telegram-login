b481a8302f0e4f80efd053437e42de7b1a8717df
NyxianNetwork <nyxiannetwork@gmail.com>
2024-08-20 08:18:45 +0700

    Update login.py

--- login.py
+++ login.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 
 # Masukkan string sesi Telegram kamu di sini
-session_string = "masukkan_string_sesi_kamu_di_sini"
+session_string = "BQGE4bMAW9xGCxFSlHg2g793J_9p2ZwHMmpDicrpa-UTSqCZvJG_fW35dpgkVMyEPtQwYH0YaVZ0X-je0UK4Tpkf78SIlnDtwUycZGPq2vcb4-PDJAxaX62L2eE8KxHBwmx3MpfUmhW85ci6-mTDdSkB3bX68ChodFQvgsFpoV0Phc86wh70LHNA_bRkLIdisgI1GCqHKiR_7ulYp20eqI3wu6XFtr-jlUCvii7PtWB1smhuv8voEOr8UOAhw6UXjgItMMw7ssf_jDzAUudWphCyBL2qBRPncBwEqn5bWFCx02Vc0aVBgo4QnGWAWrVFzita5-ca8o9iSo1PEaOnzEVLZqtg5gAAAAFfpka9AA"
 
 # Buat objek Client dengan menggunakan string sesi
 app = Client("my_account", session_string=session_string)
