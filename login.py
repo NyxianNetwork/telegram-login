fa0bc9f0efd27fcc41553c2c0fe6f3fdeb236eb6
NyxianNetwork <nyxiannetwork@gmail.com>
2024-08-20 08:13:47 +0700

    Update login.py

--- login.py
+++ login.py
@@ -1,21 +1,13 @@
-from pyrogram import Client, filters
+from pyrogram import Client
 
-# String sesi Telegram yang sudah kamu miliki
-session_string = "AQFFwxoAwX35DhN5Wq7p9tp6JJsdaGYuYJXBPj-MaEKn5nqWLlkPZ84nfkSpSTo997P5l5k7j6NqeqthZXwzthUFCl9ucgrxXCzvPayoxA75a4JehVbrztoRn9rH7Opb92wFs3dacOiJAqlNiAZ0WjsVgPVlg8iCRQUOy5OSCnGf6BfJlBTXOmnwFxmpJur7JJglSEFYo01s1zfctVPZ-nM7AzsZzsYN_nw3_HoUc4TFmwfMxe8xuzJ6w0b6ahqnUERjgly7A3CjKy7lw7vz1LTeDXmAjbkkVkH5nkOC0dbivUmDKV53llm2DzmFV2Wm6YqkxJv_FKXbxRZE2lpDePayk_UuIgAAAABs_9IyAA"
+# Gunakan string sesi yang sudah kamu miliki
+session_string = "BQGE4bMAW9xGCxFSlHg2g793J_9p2ZwHMmpDicrpa-UTSqCZvJG_fW35dpgkVMyEPtQwYH0YaVZ0X-je0UK4Tpkf78SIlnDtwUycZGPq2vcb4-PDJAxaX62L2eE8KxHBwmx3MpfUmhW85ci6-mTDdSkB3bX68ChodFQvgsFpoV0Phc86wh70LHNA_bRkLIdisgI1GCqHKiR_7ulYp20eqI3wu6XFtr-jlUCvii7PtWB1smhuv8voEOr8UOAhw6UXjgItMMw7ssf_jDzAUudWphCyBL2qBRPncBwEqn5bWFCx02Vc0aVBgo4QnGWAWrVFzita5-ca8o9iSo1PEaOnzEVLZqtg5gAAAAFfpka9AA"
 
-# Buat objek Client dengan menggunakan string sesi
+# Buat Client dengan menggunakan string sesi
 app = Client("my_account", session_string=session_string)
 
-# Menjalankan aplikasi dan menangani KeyboardInterrupt
-if __name__ == "__main__":
-    try:
-        app.start()  # Memulai Client
-        me = app.get_me()  # Memanggil `get_me()` setelah Client dimulai
-        print(f"Login sebagai: {me.first_name} ({me.phone_number})")
-        
-        print("Program sedang berjalan. Tekan CTRL + C untuk menghentikan.")
-        app.idle()  # Menjalankan event loop dan menunggu CTRL + C
-    except KeyboardInterrupt:
-        print("\nProgram dihentikan oleh pengguna.")
-    finally:
-        app.stop()  # Menghentikan Client dengan aman
+# Jalankan Client
+with app:
+    # Dapatkan informasi akun yang sedang login
+    me = app.get_me()
+    print(me)
