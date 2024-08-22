615b02f64f0182ea13381b8912ccb5270542a892
NyxianNetwork <nyxiannetwork@gmail.com>
2024-08-22 21:55:00 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from pyrogram import Client
+from config import BOT_TOKEN, API_ID, API_HASH
+import modules  # Mengimpor semua modul di direktori modules
+
+app = Client(
+    "my_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+if __name__ == "__main__":
+    app.run()
